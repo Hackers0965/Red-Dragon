692aa88e0bc59532ae8252c5a43c6cf346ad58bd
Hackers from mediafire <108221779+Hackers0965@users.noreply.github.com>
2022-06-29 23:10:16 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,9 +23,9 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+212693944029'] //Owner number in aive msg
+global.premium = ['+212693944029'] //Owner Number info
+global.ownernomer = '+212693944029' //Owner Number <<<
 
 global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
 global.botname = ' ☬RED༒DRAGON' //Bot Name
@@ -42,8 +42,8 @@ global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor In Menu!
 
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
-global.footer = '©_Kumuthu.' // DO NOT CHANGE!
+global.ig = 'https://github.com/hackers0965 ' //Github Account!
+global.footer = 'ST teen show.' // DO NOT CHANGE!
 
 global.mess = { // Error Mesages
     success: 'Done ✅',
